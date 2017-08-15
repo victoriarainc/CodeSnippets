259c9a553ee3afe41973ac285216dac5240ca192
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-15 11:57:26 -0400

    fixed styling bug in styling tags

--- public/js/snippets.js
+++ public/js/snippets.js
@@ -1,18 +1,21 @@
+// on the add/edit page
 let form = document.getElementById('snippet_form');
+// add an event listener to the snippet_form
 form.addEventListener('click', (event) => {
-
+  // if the remove button is clicked remove that element
   if (event.target.matches('button.tag_remove')) {
     event.target.parentElement.remove();
-
+  // if the add button is clicked add an additional field and button
   } else if (event.target.matches('button.tag_add')) {
     let tag_field = document.getElementById('tag_add');
     let container = document.getElementById('tag_container');
-
+    // and if a field has a value
     if (tag_field.value !== '') {
+      // display it to the page
       container.innerHTML += `
         <div class=tag_wrapper>
           <input class='add' type="array" name="tags[]" value="${tag_field.value}" />
-          <button type="button" class="tag_remove">Remove</button>
+          <button type="button" class="tag_remove tag_button">-</button>
         </div>
         `;
     }
